008e1073db6f480ca5c0b57c429849312989ae02
User <User@SERGEY>
2021-12-11 23:05:49 +0300

    Добавьте файлы проекта.

---
+++ 6 строки/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_строки
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] strArray = str.Split();
+            string word = "";
+            int z = 0;
+            foreach (String s in strArray)
+            {
+                if (s.Length > z)
+                {
+                    word = s;
+                    z = s.Length;
+                }
+            }
+                        Console.WriteLine(word);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string reverseStr = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reverseStr += str[i];
+            }
+            if (str == reverseStr)
+                Console.WriteLine("Предложение - палиндром.");
+            else
+                Console.WriteLine("Предложение - не палиндром.");
+                        Console.ReadKey();
+        }
+    }
+}
